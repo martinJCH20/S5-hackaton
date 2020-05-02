0471b6c2f3f68ae587391b554a20ba9437cb4645
martinJCH20 <61629740+martinJCH20@users.noreply.github.com>
2020-05-02 14:45:00 -0500

    Add files via upload

--- HackatonEj3.js
+++ HackatonEj3.js
@@ -8,18 +8,67 @@ class App extends Component {
 		super(props);
 		this.state = {
 			 display: false,
-             usuario: '',
              email: '',
-             password: '',
              phone: '',
+             usuario: '',
+             comentario: '',
              submitted: false,
 		};
     }
+
+    onSubmitMessage = (e) => {
+		e.preventDefault();
+		if (this.state.usuario.length === 0 || this.state.usuario.email === 0 || this.state.usuario.password === 0 || this.state.usuario.phone === 0) {
+			return;
+		} else {
+			this.setState({
+                user: this.state.usuario,
+                email: this.state.email,
+                comentario: this.state.comentario,
+                phone: this.state.phone,
+				display: true,
+				submitted: !this.state.submitted,
+			});
+        }
+    };
     
-  
+  haUserChange = (e) => {
+		this.setState({
+			usuario: e.target.value,
+		});
+    };
+
+    haEmailChange = (e) => {
+		this.setState({
+			email: e.target.value,
+		});
+    };
+
+    haComent = (e) => {
+		this.setState({
+			comentario: e.target.value,
+		});
+    };
+
+    haPhoneChange = (e) => {
+		this.setState({
+			phone: e.target.value,
+		});
+    };
+    
+    onSubmitReturn= () => {
+        this.setState({
+            usuario: '',
+            email: '',
+            comentario: '',
+            phone: '',
+            display: false,
+            submitted: !this.state.submitted,
+        });
+   }
 
 	render() {
-        const { display,user, email, passw, phone, submitted } = this.state;
+        const { display,user, email, comentario, phone, submitted } = this.state;
 		return (	
 			<div className='App'>	
 				<header className='App-header'>
@@ -27,25 +76,32 @@ class App extends Component {
                 <div className='cont-left'>
                 {!submitted ? (
                 <form className='form-register' onSubmit={this.onSubmitMessage}> 
-                <DatosUsuario></DatosUsuario>
+                <DatosUsuario title="Ingrese sus datos para el registro"
+                 usuario={this.state.usuario} 
+                 userChange={this.haUserChange}
+                 email={this.state.email} 
+                 emailChange={this.haEmailChange}
+                 comentario={this.state.comentario} 
+                 comentChange={this.haComent}
+                 phone={this.state.phone} 
+                 phoneChange={this.haPhoneChange}
+                ></DatosUsuario>
                 </form>
                 ) : null}
                 {display ? 
-                    <div>
-                     <h2 className='primary-title'>Gracias por registrar sus datos</h2>  
+                    <div className='form-register'>
+                     <h2 className='primary-title'><p>Gracias por registrar sus datos</p></h2> 
                         <div className='table-resumen'>
-                        <tr><td><label>Tus datos son los siguientes: </label></td><td></td></tr>
-                        <tr><td><label>Usuario: </label></td><td>{user}</td></tr>
-                        <tr><td><label>Email: </label></td><td>{email}</td></tr>
-                        <tr><td><label>Contraseña: </label></td><td>{passw}</td></tr>
-                        <tr><td><label>Teléfono: </label></td><td>{phone}</td></tr>
-                        <tfoot><tr>
-                        <td colSpan="2" className='foot-form'>
+                        <div><label>Tus datos son los siguientes: </label></div> <br/>
+                        <div><div className="form-img"><label>Email: </label></div><p className="result">{email}</p></div>
+                        <div><div className="form-img"><label>Teléfono: </label></div><p className="result">{phone}</p></div>
+                        <div><div className="form-img"><label>Usuario: </label></div><p className="result">{user}</p></div>
+                        <div><div className="form-img"><label>Comentario: </label></div><p className="result">{comentario}</p></div>
+                        <div>
                         <button className='primary-button'  onClick={this.onSubmitReturn}>
                         <p className='text-button'>Retornar</p>
                         </button>
-                        </td>
-                        </tr></tfoot>
+                        </div>
                         </div>
                     </div>
                 : null}
