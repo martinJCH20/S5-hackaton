1bbddc21a5340980fe4cfa2f50a85d28049018cd
martinJCH20 <61629740+martinJCH20@users.noreply.github.com>
2020-05-02 10:57:19 -0500

    Add files via upload

--- Components/Hackaton/ConversorDiv.js
+++ Components/Hackaton/ConversorDiv.js
@@ -1,28 +1,42 @@
 import React from 'react';
 
+const monedas = [
+    {id: 1, title: 'Sol Peruano', simbolo: 'S/. '},
+    {id: 2, title: 'Dolares Estadounidenses', simbolo: '$ '},
+    {id: 3, title: 'Libra', simbolo: '£ '},
+    {id: 4, title: 'Euro', simbolo: '€ '}
+  ];
+
+
+
+
 export const ConversorDiv = (props) => {
 	return props.visible ? (
 		<div className='content-form'>
             <div className='content-input'>
-                <input type="number" className="input-number" placeholder="0.00" step="0.01" min="0" />
+                <input type="number" id="input-value" className="input-number" placeholder="0.00" step="0.01" min="0" />
             </div>
             <div className='content-selections'>
                 <div className='content-venta'>
-                <select className='selection-moneda'>
+                <select className='selection-moneda' id='moneda-venta'>
                     <option value="" selected disabled>seleccionar moneda de venta</option>
+                    {monedas.map((opt) =>
+                        <option value={opt.id}>{opt.title}</option>
+                    )};
                 </select>
                 </div>
                 <div className='content-compra'>
-                <select className='selection-moneda'>
+                <select className='selection-moneda' id='moneda-compra'>
                     <option value="" selected disabled>seleccionar moneda de compra</option>
+                    {monedas.map((opt) =>
+                        <option value={opt.id}>{opt.title}</option>
+                    )};
                 </select>
                 </div>
             </div>
             <div className='content-button'>
-                <button className='primary-button'>
-                <a href={props.link}>
+                <button className='primary-button' onClick={props.activateLasers}>
                     <p className='text-button'>{props.textButton}</p>
-                </a>
                 </button>
             </div>
         </div>
