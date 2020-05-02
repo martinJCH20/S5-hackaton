e9a4810830f3fea47cbba044237b0ae3d3f65661
martinJCH20 <61629740+martinJCH20@users.noreply.github.com>
2020-05-02 13:36:12 -0500

    Add files via upload

---
+++ Components/Hackaton/FormularioContacto.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+export const DatosUsuario = (props) =>{
+    
+    return (
+    <div>
+        <h2 className='primary-title'>{props.title}</h2>
+        <div className="form-content">
+            <div className="form-img"><img src="https://img.icons8.com/ios-glyphs/30/000000/email.png"  alt=""/></div>
+            <div className='input-form'><input type="email"  value={props.email} onChange={props.emailChange}/></div>
+        </div><br />
+        <div className="form-content">
+            <div className="form-img"><img src="https://img.icons8.com/android/24/000000/phone.png" alt=""/></div>
+            <div className='input-form'><input type="tel" pattern="[0-9]{9}" value={props.phone} onChange={props.phoneChange}/></div>
+        </div><br />
+        <div className="form-content">
+            <div className="form-img"><img src="https://img.icons8.com/metro/26/000000/user-male-circle.png" alt=""/></div>
+            <div className='input-form'><input value={props.usuario} onChange={props.userChange}/></div>
+        </div><br />
+        <div className="form-content">
+            <div className="form-img"><img src="https://img.icons8.com/metro/26/000000/forgot-password.png" alt=""/></div>
+            <div className='input-form'><textarea value={props.comentario} onChange={props.comentChange}></textarea></div>
+        </div>
+        <div>
+        <button className='primary-button'>
+        <p className='text-button'>Enviar</p>
+        </button>
+        </div>
+    </div>
+    );
+}
